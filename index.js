38362300747bbac9b549819d971a115a9cc53737
vidyabharti-dev <53312210+vidyabharti-dev@users.noreply.github.com>
2025-10-07 18:56:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const admin = require("firebase-admin");
+const crypto = require("crypto");
 const cors = require("cors"); // Import CORS
 const app = express();
 const PORT = process.env.PORT || 8080;
