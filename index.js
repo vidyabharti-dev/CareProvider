8dac59ec0f0b0ccdf5ab055886c88fd437b05d2e
vidyabharti-dev <53312210+vidyabharti-dev@users.noreply.github.com>
2025-10-09 15:14:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -148,6 +148,7 @@ app.post(
     // 1. Extract inputs
     const {examid_1, examid_2} = req.params;
     const receivedCandidateKey = req.headers.candidatekey;
+    console.log(receivedCandidateKey);
 
     // --- Validation Checks ---
     if (!receivedCandidateKey) {
